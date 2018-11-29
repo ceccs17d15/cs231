ef0158188f508e9fd3160457d4033ce217defb08
ceccs17d15 <43025402+ceccs17d15@users.noreply.github.com>
2018-11-29 17:35:07 +0530

    Update bubblesort.c

--- bubblesort.c
+++ bubblesort.c
@@ -23,5 +23,16 @@ int main()
 	  printf("%d",a[i]);
 	}
 	return 0;
-}                                                                                                                   
+
+}
+output:
+user@user-HP-Laptop-15-da0xxx:~/Desktop$ gcc bubblesort.c
+user@user-HP-Laptop-15-da0xxx:~/Desktop$ ./a.out
+enter the max no of elements to sort4 
+enter the elements1
+7
+8
+2
+1278user@user-HP-Laptop-15-da0xxx:~/Desktop$ 
+
  
